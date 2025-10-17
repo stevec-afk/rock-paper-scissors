ab4b450da62075446d323ce3dfb447663734caab
stevec-afk <evets25@gmail.com>
2025-10-17 10:11:25 -0300

    Get choices and evaluate result

--- rps.js
+++ rps.js
@@ -1,42 +1,43 @@
 // Questions / things I need to look up
-// 1. How to use math.random
-// 2. Syntax for evaluating human input (Boolean logic)
-// 3. When do I use semicolons at the end of a line?
-// 4. Strings - how to convert input to lowercase
-// 5. Syntax for loops
-// 6. Which variables should be local / global?
+// - Syntax for evaluating human input (Boolean logic)
+// - Syntax for loops
+// - Which variables should be local / global?
 
-// Keep track of player scores
+// Keep track of player scores and inputs
 let playerScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+let computerChoice = 0;
 
 // Get the human choice
     // x Create a function called getHumanChoice to get the human choice
     // x GET human input using prompt() 
     // x Store input
-    // make the input case insensitive (store everything as lowercase)
+    // x make the input case insensitive (store everything as lowercase)
     // x error handling for invalid input not needed yet
-    // ? PRINT humanChoice to console.log (with standardized formatting)
+    // x PRINT humanChoice to console.log (with standardized formatting)
     // x RETURN humanChoice
 function getHumanChoice() {
     let humanChoice = prompt("Rock, paper, or scissors?");
-    // convert to lowercase here
     console.log("You chose: " + humanChoice);
-    if humanChoice == "rock" return 1;
-    else if humanChoice == "paper" return 2;
-    else if humanChoice == "scissors" return 3;
-    // Invalid choice = 0, if getHumanChoice returns 0, rerun round & do not increment counter
+    if (humanChoice.toLowerCase() === "rock") return 'r';
+    else if (humanChoice.toLowerCase() === "paper") return 'p';
+    else if (humanChoice.toLowerCase() === "scissors") return 's';
     else return 0;
 }
 
 // Get the computer choice
-    // create a function called getComputerChoice
-    // RETURN random(rock | paper | scissors)
-    // x use math.random, don't use arrays
 function getComputerChoice() {
-    // update math.random call - should return 1 2 or 3 
-    let computerChoice = math.random()
-    return computerChoice
+    // Generate a number between 0 and 2 inclusive
+    let computerChoice = Math.floor(Math.random()*3);
+    if (computerChoice === 0) return 'r';
+    else if (computerChoice === 1) return 'p';
+    else if (computerChoice === 2) return 's';
+    // This should never trigger, but just in case...
+    else{ 
+        console.log("Math Error - you should not be seeing this message.");
+        return 0;
+    }
 }
 
 // Play a single round
@@ -45,10 +46,34 @@ function getComputerChoice() {
     // EVALUATE winner
     // increment the scores based on round winner and OUTPUT round result to console
 function playRound(){
-    getComputerChoice() // store this in a variable I guess? 
-    getHumanChoice()    // store this in a variable I guess? 
-    // Insert logic to evaluate results here
+    humanChoice = getComputerChoice();
+    computerChoice = getHumanChoice();
+
+    // Result: Draw - both choices are same.
+    if (humanChoice === computerChoice){
+        // print draw message
+        // do not increment score counter
+    }
+    
+    // Result: Won - 1 of 3 scenarios
+    else if (
+        humanChoice === 'r' && computerChoice === 's' // Rock beats scissor
+        || humanChoice === 'p' && computerChoice==='r' // Paper beats rock
+        || humanChoice === 's' && computerChoice === 'p' // Scissors beats paper
+    ){
+
+        // print winner message
+        // increment score counter
+    }
+
+    // Result: Lost - if not a draw and not a win, must be a loss so no test needed
+    else {
+        // print loser message
+        // increment score counter
+    } 
 }
+
+
 
 // Play the entire game - MAIN function
     // CALL the single round function 5 times with a loop 
@@ -58,13 +83,13 @@ function playRound(){
         // output results to console.log()
     // OUTPUT final result
 
-
 // Ideas & features outside the scope of this project
-    // Let the computer "cheat" by weighting the "random" choice towards paper, 
-       // because the human player is more likely to choose rock (its the first in the list and easiest to type)
+    // Let the computer "cheat" by weighting the "random" choice towards paper, because the human player
+    //     is more likely to choose rock (its the first in the list and easiest to type)
+    // Let the player cheat with secret input options - ie.: bomb beats everything
     // Show the running score in the HTML page, and stats about games won/lost
     // Create logging so I can review how people played the game and what they chose
     // Create a web page for showing lifetime / historical player stats - leaderboard? Fancy graphs?
     // More robust error handling for human input
-    // Buttons to click on instead of relying on typed input
-    // Simplify and condense code
+    // Buttons to click on instead of relying on typed input - covered in later lesson
+    // Simplify and condense code - covered in later lesson
