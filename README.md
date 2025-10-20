c21e19b86a07e104615c6411c63082e75f788530
stevec-afk <evets25@gmail.com>
2025-10-20 10:51:14 -0300

    comments cleanup

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ A simple rock paper scissors game for TOP
 https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 
 Link to live site: https://stevec-afk.github.io/rock-paper-scissors/
+
+
+
+Ideas & features I want to add but are outside the scope of this project:
+- Let the computer "cheat" by weighting the "random" choice towards paper, because the human player is more likely to choose rock (its the first in the list and easiest to type)
+- Let the player cheat with secret input options - ie.: bomb beats everything
+- Option to unlock "advanced mode", which turns the game into rock-paper-scissors-lizard-spock
+- Show the running score in the HTML page instead of console, and show stats about games won/lost
+- Create logging so I can review how people played the game and what they chose
+- Create a web page for showing lifetime / historical player stats - leaderboard? Fancy graphs?
+- More robust error handling for typed human input (TOP project page explicitly says don't worry about this yet)
+- Buttons to click on instead of relying on typed input - covered in later lesson
+- Simplify and condense code - covered in later lesson
--- rps.js
+++ rps.js
@@ -1,23 +1,16 @@
-/* Questions / things I need to look up: 
-- None! 
-*/
-
-/* Keep track of player scores and inputs */
 let playerScore = 0;
 let computerScore = 0;
 let playerChoice = "";
 let computerChoice = 0;
 let drawScore = 0;
 
-/* Get the player choice */
 function getPlayerChoice() {
     playerChoice = prompt("Rock, paper, or scissors?"); 
     answer = playerChoice.toLowerCase(); 
-    // todo: This is where we would do error handling and account for other inputs
+    // This is where we would do error handling and account for other inputs - out of scope for now
     return answer;
 }
 
-/* Get the computer choice */
 function getComputerChoice() {
     let randnum = Math.floor(Math.random()*3); // Generate a number between 0 and 2 inclusive
     if (randnum === 0) return 'rock';
@@ -30,7 +23,6 @@ function getComputerChoice() {
     }
 }
 
-/* Play a single round */
 function playRound(){
     playerChoice = getPlayerChoice();
     computerChoice = getComputerChoice();
@@ -43,7 +35,7 @@ function playRound(){
         drawScore++
     }
     
-    // Result: Won - 1 of 3 scenarios
+    // Result: Player Won - 1 of 3 scenarios
     else if (
         playerChoice === 'rock' && computerChoice === 'scissors' // Rock beats scissor
         || playerChoice === 'paper' && computerChoice==='rock' // Paper beats rock
@@ -53,7 +45,7 @@ function playRound(){
         playerScore++;
     }
 
-    // Result: Lost - if not a draw and not a win, must be a loss so no test needed
+    // Result: Player Lost - if not a draw and not a win, must be a loss so no test needed
     else {
         console.log("Computer wins!")
         computerScore++;
@@ -61,12 +53,10 @@ function playRound(){
 }
 
 function main() {
-    // play 5 rounds
     for (let i=0; i<5; i++){
        playRound(); 
     }
     
-    //Print results
     console.log("Game completed! Results: ")
     console.log("Rounds won: " + playerScore)
     console.log("Rounds lost: " + computerScore )
@@ -76,7 +66,7 @@ function main() {
         console.log("Final result: Player wins! Congrats!");
     }
     else if (computerScore > playerScore){ 
-        console.log("Final result: Computer wins! better luck next time!");
+        console.log("Final result: Computer wins! Better luck next time!");
     }
     else { 
         console.log("Its a draw!")
@@ -86,16 +76,3 @@ function main() {
 
 main()
 
-
-/* Ideas & features outside the scope of this project:
-- Let the computer "cheat" by weighting the "random" choice towards paper, because the human player
-    is more likely to choose rock (its the first in the list and easiest to type)
-- Let the player cheat with secret input options - ie.: bomb beats everything
-- Option to unlock "advanced mode", which turns the game into rock-paper-scissors-lizard-spock
-- Show the running score in the HTML page instead of console, and show stats about games won/lost
-- Create logging so I can review how people played the game and what they chose
-- Create a web page for showing lifetime / historical player stats - leaderboard? Fancy graphs?
-- More robust error handling for human input (TOP project page explicitly says don't worry about this yet)
-- Buttons to click on instead of relying on typed input - covered in later lesson
-- Simplify and condense code - covered in later lesson
-*/
